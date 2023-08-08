f9d86d367dae390cbf2685fa84b51f29a2114d6a
fuanzang <fuanzang@qq.com>
2023-08-08 16:06:35 +0800

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -16,12 +16,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
